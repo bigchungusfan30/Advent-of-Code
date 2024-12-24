1e95ac087556ce8d1f64582a7310215dbaa0495e
bigchungusfan30 <111702790+bigchungusfan30@users.noreply.github.com>
2024-12-24 16:04:04 -0500

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,11 @@
+import re
+t = 0
+for line in open("input", "r"):
+    a = line.find("mul(")
+    for i in re.findall("mul\([0-9]+,[0-9]+\)", line):
+        comma = i.index(",")
+        firstnum = i[4:comma]
+        secondnum = i[comma+1:-1]
+        t += int(firstnum)*int(secondnum)
+
+print(t)
